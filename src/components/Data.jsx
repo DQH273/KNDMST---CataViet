42a0e235ddd85d46c9e665ebb880810326315be2
DQH273 <dangquanghao008@gmail.com>
2026-04-06 22:23:10 +0700

    Sua anh ao dai

--- src/components/Data.jsx
+++ src/components/Data.jsx
@@ -42,7 +42,7 @@ export const pics = [
     tag: "Văn hóa",
     detail:
       "Áo dài là áo dài ôm sát cơ thể, thường mặc trong dịp lễ, tết hoặc sự kiện quan trọng. Nó tượng trưng cho sự duyên dáng, thanh lịch và nét văn hóa truyền thống",
-    img: "https://catalogue-backend-cfpg.onrender.com/images/aodai2.png",
+    img: "https://catalogue-backend-cfpg.onrender.com/images/aodai.png",
   },
   {
     id: 6,
